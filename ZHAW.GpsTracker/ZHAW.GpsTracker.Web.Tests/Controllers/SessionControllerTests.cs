7b15cd100abae0e8ae800b9777e3308a2af3245c
Stefan Sidler <stefan.sidler@swisscom.com>
2015-01-28 19:08:51 +0100

    Add unit test for session controller #7

---
+++ ZHAW.GpsTracker/ZHAW.GpsTracker.Web.Tests/Controllers/SessionControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using NUnit.Framework;
+using ZHAW.GpsTracker.Web.Controllers;
+
+namespace ZHAW.GpsTracker.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class SessionControllerTests
+    {
+        [Test]
+        public void Index_WithoutSessionKey_ReturnsNotFoundResult()
+        {
+            var sut = new SessionController();
+
+            ActionResult actual = sut.Index(null);
+
+            Assert.That(actual, Is.TypeOf<HttpNotFoundResult>());
+        }
+
+        [Test]
+        public void Index_WithEmptySessionKey_ReturnsNotFoundResult()
+        {
+            var sut = new SessionController();
+
+            ActionResult actual = sut.Index(string.Empty);
+
+            Assert.That(actual, Is.TypeOf<HttpNotFoundResult>());
+        }
+
+        [Test]
+        public void Index_WithWitespaceSessionKey_ReturnsNotFoundResult()
+        {
+            var sut = new SessionController();
+
+            ActionResult actual = sut.Index(" ");
+
+            Assert.That(actual, Is.TypeOf<HttpNotFoundResult>());
+        }
+
+        [Test]
+        public void Index_WithSessionKey_ReturnsViewResult()
+        {
+            var sut = new SessionController();
+
+            ActionResult actual = sut.Index("asdf");
+
+            Assert.That(actual, Is.TypeOf<ViewResult>());
+        }
+    }
+}
