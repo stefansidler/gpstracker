b7de324a30cc73a39c0a8120aa49d2081ed1f5b2
Stefan Sidler <stefan.sidler@swisscom.com>
2015-01-28 19:59:19 +0100

    Save username in cookie #14

--- ZHAW.GpsTracker/ZHAW.GpsTracker.Web/App_Start/BundleConfig.cs
+++ ZHAW.GpsTracker/ZHAW.GpsTracker.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace ZHAW.GpsTracker.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.cookie.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/signalr").Include(
                         "~/Scripts/jquery.signalR-{version}.js"));
